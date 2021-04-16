6b1b5ca21129dd106afd3ebcfb86b9f3b6a188f3
david <david_forerope@fet.edu.co>
2021-04-15 19:49:18 -0500

    trabajando

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -1,12 +1,74 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Input, Icon, Button } from 'react-native-elements';
+import { validateEmail } from '../../utils/validations';
 export default function RegisterForm() {
+	const [showPassword, setShowPassword] = useState(false);
+	const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+	const [formData, setFormData] = useState(defaultFormValue());
+
+	const onSubmit = () => {
+		console.log(formData);
+	};
+
+	const onChange = (e, type) => {
+		setFormData({ ...formData, [type]: e.nativeEvent.text });
+	};
+
 	return (
 		<View style={styles.formContainer}>
-			<Input placeholder="Correo electronico" containerStyle={styles.inputForm} />
+			<Input
+				placeholder="Correo electronico"
+				containerStyle={styles.inputForm}
+				onChange={(e) => onChange(e, 'email')}
+				rightIcon={<Icon type="material-community" name="at" iconStyle={styles.iconRight} />}
+			/>
+			<Input
+				placeholder="Contraseña"
+				containerStyle={styles.inputForm}
+				password={true}
+				secureTextEntry={showPassword ? false : true}
+				onChange={(e) => onChange(e, 'password')}
+				rightIcon={
+					<Icon
+						type="material-community"
+						name={showPassword ? 'eye-off-outline' : 'eye-outline'}
+						iconStyle={styles.iconRight}
+						onPress={() => setShowPassword(!showPassword)}
+					/>
+				}
+			/>
+			<Input
+				placeholder="Repetir contraseña"
+				containerStyle={styles.inputForm}
+				password={true}
+				secureTextEntry={showRepeatPassword ? false : true}
+				onChange={(e) => onChange(e, 'repeatPassword')}
+				rightIcon={
+					<Icon
+						type="material-community"
+						name={showRepeatPassword ? 'eye-off-outline' : 'eye-outline'}
+						iconStyle={styles.iconRight}
+						onPress={() => setShowRepeatPassword(!showRepeatPassword)}
+					/>
+				}
+			/>
+			<Button
+				title="Unirse"
+				containerStyle={styles.btnContainerRegister}
+				buttonStyle={styles.btnRegister}
+				onPress={onSubmit}
+			/>
 		</View>
 	);
+}
+
+function defaultFormValue() {
+	return {
+		email: '',
+		password: '',
+		repeatPassword: '',
+	};
 }
 
 const styles = StyleSheet.create({
